d15a46dda3a1617523be53c3c1f1b976cd21119e
cyan <97122638+cyan-oj@users.noreply.github.com>
2023-02-05 13:54:01 -0500

    add stroke history and undo

--- src/Workspace/Workspace.jsx
+++ src/Workspace/Workspace.jsx
@@ -1,36 +1,32 @@
 import { useState, useEffect } from "react";
 import { initShaders } from "../WebGLUtils/cuon-utils.js";
 import PaletteBox, { rgbToGL } from "./Palette.jsx";
-import BrushBox from "./BrushBox.js";
 import Brushes from "./Brushes.jsx";
 import Brush from "./Brush.js";
 import Layers from "./Layers.jsx";
 import { FSHADER_SOURCE, VSHADER_SOURCE } from "../utils/shaders.js";
-import { getStroke } from "../utils/glHelpers.js";
+import { getStroke, drawPoint } from "../utils/glHelpers.js";
 import "./Workspace.css"
 
-const defaultPalette = [
-  [0, 0, 0],
-  [255, 255, 255]
-]
+const defaultPalette = [[ 0, 0, 0 ], [ 255, 255, 255 ]]
 
-const defaultBrushes = [
-  new Brush( 1, "pen" ),
-  new Brush( 5, "pen" ),
-  new Brush( 50, "pen" ),
-  new Brush( 200, "pen" )
+const defaultBrushes = [ new Brush( 1, "pen" ), new Brush( 5, "pen" ), new Brush( 50, "pen" ), new Brush( 200, "pen" )
 ]
 
 function Workspace({ name = 'untitled', height = '256', width = '256', image }) {
 
   const [ layers, setLayers ] = useState([]);
   const [ colors, setColors ] = useState(defaultPalette);
-  const [ brushes, setBrushes ] = useState(defaultBrushes)
+  const [ brushes, setBrushes ] = useState(defaultBrushes);
+
   const [ activeLayer, setActiveLayer ] = useState();
   const [ activeColor, setActiveColor ] = useState(rgbToGL( colors[0] ));
   const [ activeBrush, setActiveBrush ] = useState( defaultBrushes[0] );
 
-  const [strokeHistory, setStrokeHistory] = useState([]);
+  const [strokeHistory, setStrokeHistory] = useState({});
+  const [strokeFuture, setStrokeFuture] = useState([]);
+
+  const [ newLayerNo, setNewLayerNo ] = useState(1);
 
   const points = [];
   const position = { x: 0, y: 0 }
@@ -57,20 +53,15 @@ function Workspace({ name = 'untitled', height = '256', width = '256', image }) 
       return;
     }
 
-    const a_Position = gl.getAttribLocation( gl.program, 'a_Position' );
-    const a_PointSize = gl.getAttribLocation( gl.program, 'a_PointSize' );
-    const u_FragColor = gl.getUniformLocation( gl.program, 'u_FragColor' );
+    const glAttributes = {
+      a_Position: gl.getAttribLocation( gl.program, 'a_Position' ),
+      a_PointSize: gl.getAttribLocation( gl.program, 'a_PointSize' ),
+      u_FragColor: gl.getUniformLocation( gl.program, 'u_FragColor' )
+    }
 
     const lastPoint = JSON.parse(JSON.stringify( position ));
     const currentPoint = setPosition( event );
     const [ dist, angle ] = getStroke( lastPoint, currentPoint );
-
-    const drawPoint = ( gl, position, size, color ) => {
-      gl.vertexAttrib3f( a_Position, position[0], position[1], 0.0 );
-      gl.vertexAttrib1f( a_PointSize, size );
-      gl.uniform4f( u_FragColor, color[0], color[1], color[2], color[3] )
-      gl.drawArrays( gl.points, 0, 1 )
-    }
 
     console.log(event.pressure)
 
@@ -84,7 +75,7 @@ function Workspace({ name = 'untitled', height = '256', width = '256', image }) 
         color: activeColor
       }
 
-      drawPoint( gl, point.position, point.size, point.color )
+      drawPoint( gl, point.position, point.size, point.color, glAttributes )
       points.push( point )
     }
   }
@@ -99,7 +90,8 @@ function Workspace({ name = 'untitled', height = '256', width = '256', image }) 
     if (!gl) alert('Your browser does not support WebGL. Try using another browser, such as the most recent version of Mozilla Firefox')
     if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) console.error('failed to initialize shaders')
 
-    setLayers([...layers, { name: layerName, canvas: newCanvas, context: gl }])
+    setLayers([...layers, { id: newLayerNo, name: layerName, canvas: newCanvas, context: gl }])
+    setNewLayerNo( newLayerNo + 1 )
   }
 
   const removeLayer = () => { // todo
@@ -117,13 +109,63 @@ function Workspace({ name = 'untitled', height = '256', width = '256', image }) 
     setActiveColor( rgbToGL( color ))
   }
 
-  const saveStroke = ( points, gl ) => {
+  const undo = () => {
+    let stroke = []
+    
+    setStrokeHistory( oldStrokeHistory => {
+      const newStrokeHistory = { ...oldStrokeHistory }
+      stroke = newStrokeHistory[activeLayer.id].strokes.pop()
+      console.log("stroke", stroke)
+      return newStrokeHistory
+    })
+
+    setStrokeFuture( oldStrokeFuture => {
+      const newStrokeFuture = [...oldStrokeFuture]
+      newStrokeFuture.push({ context: activeLayer.context, points: stroke })
+      return newStrokeFuture
+    })
+
+    const gl = strokeHistory[ activeLayer.id ].context
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    strokeHistory[activeLayer.id].strokes.forEach( stroke => {
+      console.log("stroke", stroke)
+      console.log("first point", stroke[0])
+      const glAttributes = {
+        a_Position: gl.getAttribLocation( gl.program, 'a_Position' ),
+        a_PointSize: gl.getAttribLocation( gl.program, 'a_PointSize' ),
+        u_FragColor: gl.getUniformLocation( gl.program, 'u_FragColor' )
+      }
+      stroke.forEach( point => {
+        drawPoint( gl, point.position, point.size, point.color, glAttributes )
+      })
+    })
+  }
+
+  // {
+  //   0: {
+  //          context: gl, 
+  //          points: [[points], [points]] 
+  //      },
+  //   1: {
+  //          context: gl, 
+  //          points: [[points], [points]] 
+  //      },
+  //   2: {
+  //          context: gl, 
+  //          points: [[points], [points]] 
+  //      },
+  // }
+
+  const saveStroke = ( points, layer ) => {
     if (points.length > 0) {
-      setStrokeHistory([...strokeHistory, {
-        points: points,
-        context: gl
-      }])
+      setStrokeHistory( oldStrokeHistory => {
+        const newStrokeHistory = { ...oldStrokeHistory }
+        newStrokeHistory[layer.id] ? newStrokeHistory[layer.id].strokes.push(points) : newStrokeHistory[layer.id] = { context: layer.context, strokes: [points] }
+        return newStrokeHistory
+      })
     }
+    console.log(strokeHistory)
   }
 
   const attachLayers = () => {
@@ -137,6 +179,7 @@ function Workspace({ name = 'untitled', height = '256', width = '256', image }) 
     <div className="workspace" id={name}>
       <div className="tools">
         <h1>minxel</h1>
+        <button onClick={ undo }>undo</button>
         <PaletteBox colors={ colors } setColors={ setColors } setColor={ setColor } />
         <Brushes brushes={ brushes } setBrushes={ setBrushes } setBrush={ setBrush }/>
         <Layers layers={ layers } setLayers={ setLayers } addLayer={ addLayer } setLayer={ setLayer } points={ points }/>
@@ -145,8 +188,8 @@ function Workspace({ name = 'untitled', height = '256', width = '256', image }) 
         onPointerDown={ setPosition } 
         onPointerEnter={ setPosition }
         onPointerMove={ e => draw(e, activeLayer.context) }
-        onPointerUp={ e => saveStroke( points, activeBrush, activeColor, activeLayer.context ) }
-        onPointerLeave={ e => saveStroke( points, activeBrush, activeColor, activeLayer.context ) }
+        onPointerUp={ e => saveStroke( points, activeLayer ) }
+        onPointerLeave={ e => saveStroke( points, activeLayer ) }
       />
     </div>
   )
--- src/utils/glHelpers.js
+++ src/utils/glHelpers.js
@@ -3,3 +3,10 @@ export const getStroke = (point1, point2) => {
   const angle = Math.atan2( point2.x - point1.x, point2.y - point1.y );
   return [distance, angle]
 }
+
+export const drawPoint = ( gl, position, size, color, glAttributes ) => {
+  gl.vertexAttrib3f( glAttributes.a_Position, position[0], position[1], 0.0 );
+  gl.vertexAttrib1f( glAttributes.a_PointSize, size );
+  gl.uniform4f( glAttributes.u_FragColor, color[0], color[1], color[2], color[3] )
+  gl.drawArrays( gl.points, 0, 1 )
+}
